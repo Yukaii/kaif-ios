ed4a1a3b3730ce5a8b1ab5c9e522ab384bbdee52
Yukai Huang <yukaihuang1993@hotmail.com>
2016-01-30 21:55:59 +0800

    parse debate markdown

--- src/components/Debate.js
+++ src/components/Debate.js
@@ -2,8 +2,18 @@ import React, {
   View,
   TouchableHighlight,
   Text,
-  ActionSheetIOS
+  ActionSheetIOS,
+  LinkingIOS
 } from 'react-native';
+
+import HTMLWebView from 'react-native-html-webview';
+import marked from 'marked';
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true
+});
 
 let Debate = React.createClass({
   _handleDebateLongPress(event) {
@@ -32,6 +42,39 @@ let Debate = React.createClass({
     onDebateReply(debate);
   },
 
+  renderMarkdown(md) {
+    let parsedHTML = marked(md);
+    let regex = /<a\s+(?:[^>]*?\s+)?href="([^"]*)"/g
+    var m, links = [];
+    while (m = regex.exec(parsedHTML)) {
+        m.shift();
+        links = links.concat(m);
+    }
+
+    return `
+    <html>
+      <head>
+        <style>
+          html {
+            background-color: #EEEEEE;
+            font-size: 14px;
+            font-family: Helvetica;
+            word-break: break-all;
+          }
+          a {
+            color: #2081e4;
+          }
+        </style>
+      </head>
+      <body>
+        ${parsedHTML}
+        <p>
+          ${links.map((href, i) => `[${i+1}]: <a href=${href}>${href}</a><br/>`)}
+        </p>
+      </body>
+    </html>`;
+  },
+
   render() {
     const {debate, onDebateReply} = this.props;
 
@@ -39,7 +82,7 @@ let Debate = React.createClass({
       <TouchableHighlight
         underlayColor="#eeeeee"
         onLongPress={this._handleDebateLongPress}
-        onPress={this._onDebatePress}
+        // onPress={this._onDebatePress}
       >
         <View
           style={{flex: 1, backgroundColor: '#eeeeee', marginBottom: 6}}>
@@ -50,7 +93,13 @@ let Debate = React.createClass({
             </View>
             <Text style={{color: '#777777'}}>{debate.lastUpdateTimeFromNow()}</Text>
           </View>
-          <Text>{debate.content}</Text>
+          <HTMLWebView
+            html={this.renderMarkdown(debate.content)}
+            makeSafe={false}
+            autoHeight={true}
+            style={{marginBottom: -10, backgroundColor: '#EEEEEE'}}
+            onLink={(href) => {LinkingIOS.openURL(href)}}
+          />
         </View>
       </TouchableHighlight>
     );
